f2180ec77f12bf7a947bfcef89898e5e412defda
simongm2011 <130514985+simongm2011@users.noreply.github.com>
2024-02-26 18:20:04 -0500

    Update FrmPrincipal.java

--- src/codigo/FrmPrincipal.java
+++ src/codigo/FrmPrincipal.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package codigo;
 
 import java.io.BufferedReader;
@@ -15,10 +11,7 @@ import java.io.Reader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Charly Ponce
- */
+
 public class FrmPrincipal extends javax.swing.JFrame {
 
     /**
